9c09a9965c362d3a2b07542199a5534565f3c66d
24LP2417 24LP2417 <24LP2417@G301-CC0200.local>
2025-04-07 12:35:32 +0800

    back end general files created

--- back-end/src/back.js
+++ back-end/src/back.js
@@ -1,1 +1,17 @@
-console.log("back end")
+import express from "express"
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+const port = 3001;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(() => {
+  console.log("database connenction established");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
