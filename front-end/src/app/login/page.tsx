a3395d739a78d27d48c4b458ed610c91f5748460
enkhjin9127 <ka0enkhjin@gmail.com>
2025-04-07 11:58:57 +0800

    login

---
+++ front-end/src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Coffee } from "lucide-react";
+const Login = () => {
+  return (
+    <div className="flex w-full h-screen">
+      <div className="bg-amber-400 w-1/2 h-full">
+        <div>
+          <Coffee />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Login;
