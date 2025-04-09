fe6ca6cdc3db1f58cbb118096fbc423344bbbbbe
24LP2417 24LP2417 <24LP2417@G301-CC0200.local>
2025-04-09 13:04:05 +0800

    login completed

--- back-end/src/resolvers/users/getUser.ts
+++ back-end/src/resolvers/users/getUser.ts
@@ -1,12 +1,16 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// export const getUser = async (req, res) => {
-//   const prisma = new PrismaClient()
-//   try {
-//     const { _id } = req.user;
-//     const User = await prisma.user.findById(_id);
-//     res.json({ message: "get user data", user: User });
-//   } catch (err) {
-//     res.status(403).json({ message: "Error occurred" });
-//   }
-// };
+export const getUser = async (req, res) => {
+  const prisma = new PrismaClient();
+  try {
+    const { id } = req.user;
+    const User = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    res.json({ message: "get user data", user: User });
+  } catch (err) {
+    res.status(403).json({ message: "Error occurred" });
+  }
+};
--- back-end/src/routers/userRouter.ts
+++ back-end/src/routers/userRouter.ts
@@ -1,12 +1,13 @@
 import express from "express";
 // import {getUser} from "../resolvers/users/getUser"
-// import { login } from "../resolvers/users/login";
-import { validateEmail } from "../middleWares/validateEmail";
+// import { validateEmail } from "../middleWares/validateEmail";
 import { authorizationMiddleware } from "../middleWares/authorizationUsers";
 import { createUser } from "../resolvers/users/createUser";
+import { login } from "../resolvers/users/login";
+import { getUser } from "../resolvers/users/getUser";
 
 export const userRouter = express.Router();
 
 userRouter.post("/signup", createUser);
 // userRouter.get("/", authorizationMiddleware, getUser);
-// userRouter.post("/login", validateEmail, login);
+userRouter.post("/login", login);
