01f0942d8230ad2350998595ee819f5827c003ad
24LP2417 24LP2417 <24LP2417@G301-CC0200.local>
2025-04-10 10:37:49 +0800

    profile changed

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { userRouter } from "./routers/userRouter";
+import { profileRouter } from "./routers/profileRouter";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
--- back-end/src/resolvers/profile/createProfile.ts
+++ back-end/src/resolvers/profile/createProfile.ts
@@ -1,32 +1,61 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
 export const createProfile = async (req: Request, res: Response) => {
-  const {name, about,  avatarImage, backgroundImage,successMessage, User,SocialMediaURL} = req.body;
-  const prisma = new PrismaClient();
-
   try {
-    let profile: Prisma.ProfileCreateInput;
+    const { name, about, avatarImage, successMessage, SocialMediaURL, userId } =
+      req.body;
+    const prisma = new PrismaClient();
 
-    profile = {
-      name:name, 
-      about:about,  
-      avatarImage:avatarImage, 
-      backgroundImage:backgroundImage,
-      successMessage:successMessage,
-      SocialMediaURL:SocialMediaURL,
-      User:User
-    };
-
-    const createProfile = await prisma.profile.create({ data: profile });
-
-    res.json({
-      message: "success",
-      profile: createProfile,
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
     });
-  } catch (error) {
-    console.log(error);
-    res.status(403).json({ message: "Error occurred" });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const existingProfile = await prisma.profile.findUnique({
+      where: { userId: userId },
+    });
+
+    if (existingProfile) {
+      return res.status(404).json({ message: "Profile is already existed" });
+    }
+
+    const profile = await prisma.profile.create({
+      data: {
+        name: name,
+        about: about,
+        avatarImage: avatarImage,
+        successMessage: successMessage,
+        SocialMediaURL: SocialMediaURL,
+        User: {
+          connect: userId,
+        },
+      },
+      include: {
+        User: {
+          select: {
+            id: true,
+            email: true,
+            username: true,
+          },
+        },
+      },
+    });
+
+    res.status(201).json({
+      message: "Profile created successfully",
+      profile,
+    });
+  } catch (err) {
+    console.log();
+    res.status(500).json({
+      error: "Internet server error",
+      message: "Profile creation failed",
+    });
   }
 };
-
