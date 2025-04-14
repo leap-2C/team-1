dc153e3cdec38f43b4baba979ae954527d332790
enkhjin9127 <ka0enkhjin@gmail.com>
2025-04-14 10:17:14 +0800

    Did Dashboard

---
+++ front-end/src/app/explore/page.tsx
@@ -0,0 +1,11 @@
+import DashBoard from "@/components/DashBoard";
+export default function Explore() {
+  return (
+    <div>
+      <div>
+        <DashBoard />
+      </div>
+      <div>hello</div>;
+    </div>
+  );
+}
---
+++ front-end/src/components/DashBoard.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ExternalLink, Home, Settings, Compass } from "lucide-react";
+
+export default function DashBoard() {
+  return (
+    <div className="relative">
+      <div className="w-[251px] h-[156px] absolute top-[100px] left-[80px] ">
+        <Link href="/home">
+          <Button variant="ghost" className="justify-start w-full">
+            <Home className="mr-2 h-4 w-4" />
+            Home
+          </Button>
+        </Link>
+
+        <Button variant="ghost" className="justify-start w-full bg-muted">
+          <Compass className="mr-2 h-4 w-4" />
+          Explore
+        </Button>
+
+        <Link href="/view" target="_blank" rel="noopener noreferrer">
+          <Button variant="ghost" className="justify-start w-full">
+            <ExternalLink className="mr-2 h-4 w-4" />
+            View page
+          </Button>
+        </Link>
+
+        <Link href="/settings">
+          <Button variant="ghost" className="justify-start w-full">
+            <Settings className="mr-2 h-4 w-4" />
+            Account settings
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
