edbb5782956c5d69ae078eec2a91ee52392aca7b
BALKNAoppa <baterdene207@gmail.com>
2025-04-14 12:31:52 +0800

    homepage enhancement

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import DashBoard from '@/components/DashBoard'; 
+import HomePage from '@/components/home_page/HomePage';
+
 
 export default function Home() {
   return (
-  <div className="flex flex-col items-center justify-center h-screen">
-    <div className="w-full h-screen">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Our App</h1>
-        <p className="text-lg mb-8">
-          This is a simple app to demonstrate the use of Next.js and Tailwind CSS.
-        </p>
-      </div>
-    </div>
+    <div className="max-w-[1440px] mx-auto">
+      <DashBoard/>
+      <HomePage/>
     </div>
   );
 }
--- front-end/src/components/DashBoard.tsx
+++ front-end/src/components/DashBoard.tsx
@@ -4,27 +4,24 @@ import { ExternalLink, Home, Settings, Compass } from "lucide-react";
 
 export default function DashBoard() {
   return (
-    <div className="relative">
-      <div className="w-[251px] h-[156px] absolute top-[100px] left-[80px] ">
-        <Link href="/home">
+    <div className="relative ">
+      <div className="w-[251px] h-[156px] absolute top-[44px] left-[80px]">
+        <Link href="/">
           <Button variant="ghost" className="justify-start w-full">
             <Home className="mr-2 h-4 w-4" />
             Home
           </Button>
         </Link>
-
-        <Button variant="ghost" className="justify-start w-full bg-muted">
+        <Button variant="ghost" className="justify-start w-full">
           <Compass className="mr-2 h-4 w-4" />
           Explore
         </Button>
-
         <Link href="/view" target="_blank" rel="noopener noreferrer">
           <Button variant="ghost" className="justify-start w-full">
             <ExternalLink className="mr-2 h-4 w-4" />
             View page
           </Button>
         </Link>
-
         <Link href="/settings">
           <Button variant="ghost" className="justify-start w-full">
             <Settings className="mr-2 h-4 w-4" />
